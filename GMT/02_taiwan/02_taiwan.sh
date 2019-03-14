9efecd047d518f8f75f1ef428481f44c7863049b
chieh89 <yeahhhhh0814@gmail.com>
2019-03-14 20:19:07 +0800

    2019.03.14

---
+++ GMT/02_taiwan/02_taiwan.sh
@@ -0,0 +1,19 @@
+# set file name
+out_ps=02_taiwan.ps
+  
+# reset output paper size to A4
+gmt set PS_MEDIA A4
+  
+# start gmt session
+gmt psxy -R0/1/0/1 -JX1c -T -K -P > $out_ps
+  
+# write your gmt code here
+gmt pscoast -R119/123/21/26 -Jm4 -B1 -W1 -Df -Ia -P -O -K >> $out_ps
+  
+# end gmt session
+gmt psxy -R -J -O -T >> $out_ps
+  
+# convert to pdf
+gmt psconvert $out_ps -P -Tf
+# convert to png
+gmt psconvert $out_ps -P -Tg
